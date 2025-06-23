e514c8cd2a316d0c70ef9b2439b2587b674b2847
James <26018087+Jameschan215@user.noreply.github.com>
2025-06-23 09:49:35 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import { createServer } from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+
+const PORT = process.env.PORT || 3000;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = createServer(async (req, res) => {
+	let filePath = '';
+
+	switch (req.url) {
+		case '/':
+			filePath = path.join(__dirname, 'views', 'index.html');
+			res.statusCode = 200;
+			break;
+		case '/about':
+			filePath = path.join(__dirname, 'views', 'about.html');
+			res.statusCode = 200;
+			break;
+		case '/contact-me':
+			filePath = path.join(__dirname, 'views', 'contact-me.html');
+			res.statusCode = 200;
+			break;
+		case '/contact':
+			res.setHeader('Location', '/contact-me');
+			res.statusCode = 302;
+			break;
+		default:
+			filePath = path.join(__dirname, 'views', '404.html');
+			res.statusCode = 404;
+			break;
+	}
+
+	res.setHeader('Content-Type', 'text/html');
+	const fileContent = await fs.readFile(filePath);
+	res.write(fileContent);
+	res.end();
+});
+
+server.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
